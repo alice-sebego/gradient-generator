b98d41aced05527d367eee91cf90d3d5e1e0bb0c
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-18 16:42:58 +0200

    Improve user's graphic interface - WIP

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// Elements of DOM
+const $color1 = document.querySelector("#color-1");
+const $color2 = document.querySelector("#color-2");
+const $range = document.querySelector("#id");
+
+const $addBtn = document.querySelector("#buttons > button:nth-child(1)");
+const $subtractBtn = document.querySelector("#buttons > button:nth-child(2)");
+const $random = document.querySelector("#buttons > button:nth-child(3)");
+
