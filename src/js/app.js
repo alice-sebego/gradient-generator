7f165bad56f1e11100812c087493495063319173
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-19 17:01:22 +0200

    Handle initialisation of gradient's display

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,31 @@
 // Elements of DOM
-const $color1 = document.querySelector("#color-1");
-const $color2 = document.querySelector("#color-2");
-const $range = document.querySelector("#id");
+const $body = document.querySelector("body");
+const $inputsColor = document.querySelectorAll("#colors > input[type='color']");
+const $range = document.querySelector("#deg");
+console.log($range.value);
 
 const $addBtn = document.querySelector("#buttons > button:nth-child(1)");
 const $subtractBtn = document.querySelector("#buttons > button:nth-child(2)");
 const $random = document.querySelector("#buttons > button:nth-child(3)");
+
+// Initialisation
+
+let colorsList = ["#3a3aa1", "#37907a"]
+$inputsColor[0].value = colorsList[0];
+$inputsColor[1].value = colorsList[1];   
+
+$body.style.background = `linear-gradient(${$range.value}deg, ${$inputsColor[0].value}, ${$inputsColor[1].value}) no-repeat center center fixed`;
+$body.style.backgroundSize = "cover";
+
+
+$inputsColor.forEach( color => {
+    
+    color.addEventListener("change", e =>{
+        color.value = e.target.value;
+        console.log(color.value);
+        console.log(colorsList)
+    });
+
+});
+   
 
