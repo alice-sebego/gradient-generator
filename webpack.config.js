849a5da68501c799b0219ba2f159102b0736fbae
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-20 17:31:27 +0200

    Install Babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,19 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+  
 };
