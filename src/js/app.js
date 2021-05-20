8ec671d1b0d9c591830476a23b941d59cbabe482
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-20 15:28:43 +0200

    Refactoring events in order to add and subtract inputs

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ const $inputsColor = document.querySelectorAll("#colors > fieldset > input[type=
 const $range = document.querySelector("#deg");
 console.log($range.value);
 
+const $btns = document.querySelectorAll("#buttons > button")
 const $addBtn = document.querySelector("#buttons > button:nth-child(1)");
 const $subtractBtn = document.querySelector("#buttons > button:nth-child(2)");
 const $random = document.querySelector("#buttons > button:nth-child(3)");
@@ -18,64 +19,7 @@ $inputsColor[1].value = colorsList[1];
 
 $body.style.background = `linear-gradient(${$range.value}deg, ${colorsList}) no-repeat center center fixed`;
 
-// Add a color on colors array
-$addBtn.addEventListener("click", () => {
-
-    const $allInputs = document.querySelectorAll("#colors > fieldset > input[type='color']");
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    
-    if ($allInputs.length > 7 ){
-
-        return;
-   
-    } else {
-
-        const newColor = document.createElement("input");
-        newColor.setAttribute("type", "color");
-        newColor.setAttribute('data-index', index);
-        newColor.value = `#${randomColor.toUpperCase()}`; 
-        $fieldset.appendChild(newColor);
-
-        colorsList.push(`#${randomColor.toUpperCase()}`);
-        
-        $body.style.background = `linear-gradient(${$range.value}deg, ${colorsList}) no-repeat center center fixed`;
-        
-        index++;
-        console.log(colorsList);
-        console.log($allInputs.length);
-
-        //$allInputs.forEach( input => input.addEventListener("input", updateColor));
-    }
-
-
-});
-
-// Subtract a color on colors array
-$subtractBtn.addEventListener("click", () => {
-
-    const $allInputs = document.querySelectorAll("#colors > fieldset > input[type='color']");
-    
-    if ($allInputs.length < 3){
-
-        return;
-        
-    } else {
-        
-        colorsList.pop();
-        $allInputs[$allInputs.length - 1].remove();
-        index--;
-        $body.style.background = `linear-gradient(${$range.value}deg, ${colorsList}) no-repeat center center fixed`;     
-        
-        //$allInputs.forEach( input => input.addEventListener("input", updateColor));
-    
-    }
-
-    
-});
-
 // Listening change of input color - WIP
-
-
 const updateColor = e => {
 
     let currentValue = e.target.value;
@@ -88,3 +32,60 @@ const updateColor = e => {
 }
 
 $inputsColor.forEach( input => input.addEventListener("input", updateColor));
+
+// Add or subtract a color on colors array
+const addOrSubtractInput = (e) => {
+
+    const $allInputs = document.querySelectorAll("#colors > fieldset > input[type='color']");
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    if(e.target.value === "add"){
+
+        if ($allInputs.length > 7 ){
+
+            return;
+       
+        } else {
+    
+            const newColor = document.createElement("input");
+            newColor.setAttribute("type", "color");
+            newColor.setAttribute('data-index', index);
+            newColor.value = `#${randomColor.toUpperCase()}`; 
+            $fieldset.appendChild(newColor);
+    
+            colorsList.push(`#${randomColor.toUpperCase()}`);
+            
+            $body.style.background = `linear-gradient(${$range.value}deg, ${colorsList}) no-repeat center center fixed`;
+            
+            index ++;
+            
+            const $allInputs = document.querySelectorAll("#colors > fieldset > input[type='color']");
+           
+            $allInputs.forEach( input => input.addEventListener("input", updateColor));
+        }
+    
+    } else if(e.target.value === "subtract"){
+
+        const $allInputs = document.querySelectorAll("#colors > fieldset > input[type='color']");
+
+        if ($allInputs.length < 3){
+
+            return;
+            
+        } else {
+            
+            colorsList.pop();
+            
+            $allInputs[$allInputs.length - 1].remove();
+            index --;
+            $body.style.background = `linear-gradient(${$range.value}deg, ${colorsList}) no-repeat center center fixed`;     
+        
+        }
+
+    }
+    
+}
+
+$btns.forEach( btn => {
+    btn.addEventListener("click", addOrSubtractInput );
+});
